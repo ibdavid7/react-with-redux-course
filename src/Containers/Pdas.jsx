68f1147d54b02402cf23e18454e7259c3d06980e
David <ibdavid@gmx.net>
2023-02-27 00:15:39 -0600

    antd Card and Pda container updated

--- src/Containers/Pdas.jsx
+++ src/Containers/Pdas.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import {pda} from "../Data/dummy";
-import {Avatar,  Card} from 'antd';
+import {Avatar, Card} from 'antd';
 
 const {Meta} = Card;
 
 const Pdas = () => {
     return (
 
-        <div className={'flex flex-col flex-wrap items-stretch'}>
-            <div className={'flex flex-row grow-[10] border p-8 m-10'}>
+        <div className={'flex flex-col'}
+             style={{display: 'flex', backgroundColor: 'rgb(249 250 251)', borderRadius: '10px'}}>
+            <div className={'p-8 m-10 bg-slate-400'}
+                 style={{
+                     padding: '10px 0 10px 0',
+                     color: 'white', backgroundColor: 'rgb(24 24 27)',
+                     display: 'flex', justifyContent: 'center',
+                     alignItems: 'center', borderTopRightRadius: '10px', borderTopLeftRadius:'10px'
+                 }}>
                 <h1>Personal Digital Assistants</h1>
             </div>
 
-            <div className={'flex flex-row flex-wrap p-8'} style={{display: 'flex', flexDirection: 'row', flexWrap:'wrap', justifyContent: 'space-between'}}>
+            <div className={'flex flex-row flex-wrap p-8'}
+                 style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between'}}>
                 {pda.map(({img, handle, title, description}, index) => {
                     return (
                         <div style={{padding: '10px'}}>
-                        <Card
-                            extra={<a href="#">More</a>}
-                            key={index}
-                            hoverable
-                            style={{width: 240}}
-                            cover={<img alt={title} src={img} style={{objectFit:'contain', maxHeight:'225px'}}/>}
-                            loading={false}
-                        >
-                            <Meta
-                                title={title}
-                                handle={handle}
-                                description={description}
-                                avatar={<Avatar src="https://joesch.moe/api/v1/random"/>}
-                            />
-                            <div className="additional">
-                                <p className="price">Price: 20$</p>
-                                <p>Author: John Doe</p>
-                            </div>
-                        </Card>
+                            <Card
+                                extra={<a href="#">More</a>}
+                                key={index}
+                                hoverable
+                                style={{width: 240}}
+                                cover={<img alt={title} src={img} style={{objectFit: 'contain', maxHeight: '225px'}}/>}
+                                loading={false}
+                            >
+                                <Meta
+                                    title={title}
+                                    handle={handle}
+                                    description={description}
+                                    avatar={<Avatar src="https://joesch.moe/api/v1/random"/>}
+                                />
+                            </Card>
                         </div>
                     );
                 })}
